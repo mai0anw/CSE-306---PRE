db556b9968d21c11d5c8d4b5fa23a2277c3253f4
Lud0vici <zenglouis9@gmail.com>
2025-02-22 20:43:25 -0500

    Update tests.c

--- tests.c
+++ tests.c
@@ -1,16 +1,17 @@
 #include <criterion/criterion.h>
 #include "csv.h" // Include the header of your csv.c file
 
-Test(sample_test, test_name) {
-    printf("Test is running...\n");
-    cr_assert_eq(1, 1, "Should pass");
-}
-
-// Test(min_field, valid_field) {
-//     // Test min_field with a valid field
-//     int result = min_field(2, "data.csv");
-//     cr_assert_eq(result, expected_min_value, "The minimum value should be correct.");
+// Test(sample_test, test_name) {
+//     printf("Test is running...\n");
+//     cr_assert_eq(1, 1, "Should pass");
 // }
+
+Test(min_field, valid_field) {
+    // Test min_field with a valid field
+    int result = min_field(0, "05020004-eng.csv");
+    int expected_mean_min_value = 1998;
+    cr_assert_eq(result, expected_min_value, "The minimum value should be correct.");
+}
 
 // Test(mean_field, valid_field) {
 //     // Test mean_field with a valid field
