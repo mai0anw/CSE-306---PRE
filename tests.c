68fbaac901498f9962bd277493c00a8ef3e7b8df
Lud0vici <zenglouis9@gmail.com>
2025-02-22 21:11:37 -0500

    Update tests.c

--- tests.c
+++ tests.c
@@ -2,10 +2,10 @@
 #include "csv.h" // Include the header of your csv.c file
 
 
-int runTest_min_fields(int field_index, const char *filename, int expected) {
-    int actual = min_fields(field_index, filename);
+int runTest_min_field(int field_index, const char *filename, int expected) {
+    int actual = min_field(field_index, filename);
     cr_assert_eq(actual, expected, "count_fields(\"%s\") should have produced \"%d\", but returned \"%d\" instead.\n", filename, expected, actual);
 
 }
 
-Test(PRE, test00) { runTest_min_fields("05020004-eng.csv", 8); }
+Test(PRE, test00) { runTest_min_field("05020004-eng.csv", 8); }
