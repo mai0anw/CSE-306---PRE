caf9bab43e1e3a3c5cc4a83ec3cc8b8dd7131998
Lud0vici <zenglouis9@gmail.com>
2025-02-22 21:05:29 -0500

    Update tests.c

--- tests.c
+++ tests.c
@@ -2,7 +2,7 @@
 #include "csv.h" // Include the header of your csv.c file
 
 
-void runTest_count_fields(const char *filename, int expected) {
+int runTest_count_fields(const char *filename, int expected) {
     int actual = count_fields(filename);
     cr_assert_eq( actual, expected, "count_fields(\"%s\") should have produced \"%d\", but returned \"%d\" instead.\n", filename, expected, actual);
 
