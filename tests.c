45e076b330b28d6b519430fc818b545697e14140
Lud0vici <zenglouis9@gmail.com>
2025-02-22 20:56:44 -0500

    Update tests.c

--- tests.c
+++ tests.c
@@ -1,20 +1,11 @@
 #include <criterion/criterion.h>
 #include "csv.h" // Include the header of your csv.c file
 
-// Test(sample_test, test_name) {
-//     printf("Test is running...\n");
-//     cr_assert_eq(1, 1, "Should pass");
-// }
 
-Test(min_field, valid_field) {
-    // Test min_field with a valid field
-    int result = min_field(0, "05020004-eng.csv");
-    int expected_min_value = 1998;
-    cr_assert_eq(result, expected_min_value, "The minimum value should be correct.");
+void runTest_count_fields(const char *filename, int expected) {
+    int actual = count_fields(filename);
+    cr_assert_eq( actual, expected, "count_fields(\"%s\") should have produced \"%d\", but returned \"%d\" instead.\n", in, expected, actual);
+
 }
 
-// Test(mean_field, valid_field) {
-//     // Test mean_field with a valid field
-//     double result = mean_field(3, "data.csv");
-//     cr_assert_eq(result, expected_mean_value, "The mean value should be correct.");
-// }
+Tests(PRE, test00) { runTest_count_fields("05020004-eng.csv", 8); }
